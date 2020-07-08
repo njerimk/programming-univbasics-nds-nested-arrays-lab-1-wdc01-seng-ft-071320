36bae3a9c177c5378e1331db965824970cf7c3c1
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-08 16:18:23 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -113,7 +113,7 @@ def matrix_update(matrix, row, column, new_value)
       "Asparagus"
   ]
 ] 
-puts storage_room[row][column]=new_value
+puts matrix[row][column]=new_value
 puts storage_room[row]
 
 end
